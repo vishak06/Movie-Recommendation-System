fed80517f31369057f599e209c4d34a0feeac20c
vishak06 <vj8506@gmail.com>
2025-12-29 12:50:23 +0530

    static

--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,5 @@ pip install -r requirements.txt
 # Run preprocessing to generate the pickle file
 python recommendation/preprocess_data.py
 
-# Collect static files
-python manage.py collectstatic --noinput --clear
+# Collect static files (skip if database is not available)
+python manage.py collectstatic --noinput --clear 2>/dev/null || echo "Skipping collectstatic due to missing database"
--- movie/settings.py
+++ movie/settings.py
@@ -36,22 +36,15 @@ CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='https://*.vercel.
 
 INSTALLED_APPS = [
     'recommendation',
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
